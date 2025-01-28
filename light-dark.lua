d03dd9e2d886ed8c6019fa4ebe99bf506176ba57
Gordon Woodhull <gordon.woodhull@posit.co>
2025-01-27 20:56:48 -0500

    use classes
    not as huge of a win for R because we still rely on filenames to get here
    but still a little sigh of relief

--- light-dark.lua
+++ light-dark.lua
@@ -2,6 +2,8 @@ function Image(img)
   if img.src:match '%.light%.svg' then
     img2 = img:walk {}
     img2.src = img.src:gsub('%.light%.svg', '.dark.svg')
-    return pandoc.Inlines({img, img2})
+    img.classes:insert 'quarto-light-image'
+    img2.classes:insert 'quarto-dark-image'
+  return pandoc.Inlines({img, img2})
   end
 end

4d2312b2439086e013744afd416812f2079e1b1a
Gordon Woodhull <gordon.woodhull@posit.co>
2025-01-30 00:12:14 -0500

    use .quarto-light-content and .quarto-dark-content

--- light-dark-knitr.lua
+++ light-dark-knitr.lua
@@ -2,8 +2,8 @@ function Image(img)
   if img.src:match '%.light%.svg' then
     img2 = img:walk {}
     img2.src = img.src:gsub('%.light%.svg', '.dark.svg')
-    img.classes:insert 'quarto-light-image'
-    img2.classes:insert 'quarto-dark-image'
+    img.classes:insert 'quarto-light-content'
+    img2.classes:insert 'quarto-dark-content'
   return pandoc.Inlines({img, img2})
   end
 end
